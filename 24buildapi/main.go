80c34356ecff6b7fba0ee2add9d6d6c6d605ee77
surya kant <suryakant0013@yahoo.in>
2023-07-02 00:39:35 +0530

    added getOneCourse method

--- 24buildapi/main.go
+++ 24buildapi/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
@@ -39,4 +40,23 @@ func getAllCourses(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Get all courses")
 	w.Header().Set("Content-Type", "Application/json")
 	json.NewEncoder(w).Encode(courses)
+}
+
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("get one course")
+	w.Header().Set("Content-Type", "Application/json")
+
+	//grab id from request
+
+	params := mux.Vars(r)
+
+	// loop through courses, find matching id and return the response
+	for _, course := range courses {
+		if course.CourseId == params["id"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("No Course found with given id")
+	return
 }
