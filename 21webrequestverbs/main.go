5457467e21426863b894778e4759e3a070188143
surya kant <suryakant0013@yahoo.in>
2023-07-01 17:58:00 +0530

    get request in go lang

---
+++ 21webrequestverbs/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	fmt.Println("welcome to web request verbs")
+	PerformGetRequest()
+}
+
+func PerformGetRequest() {
+	const myurl = "http://localhost:8000/get"
+
+	response, err := http.Get(myurl)
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+
+	fmt.Println("status code : ", response.StatusCode)
+	fmt.Println("content length is : ", response.ContentLength)
+
+	var responseString strings.Builder
+
+	content, _ := ioutil.ReadAll(response.Body)
+	byteCount,_:= responseString.Write(content)
+
+	fmt.Println("ByteCount is : ",byteCount)
+	fmt.Println(responseString.String())
+
+	// fmt.Println("content : ", string(content))
+}
