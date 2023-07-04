bb4dc2e284f03bb4bc9dee2be053f34e2d858380
surya kant <suryakant0013@yahoo.in>
2023-07-04 23:40:07 +0530

    added wait group to manage go routines

--- 26goroutines/main.go
+++ 26goroutines/main.go
@@ -2,17 +2,48 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"net/http"
+	"sync"
+	// "time"
 )
 
+var wg sync.WaitGroup // use pointer in future
+
 func main() {
-	go greeter("Hello")
-	greeter("World")
+	// go greeter("Hello")
+	// greeter("World")
+
+	
+	websiteList := []string{
+		"https://lco.dev",
+		"https://go.dev",
+		"https://google.com",
+		"https://fb.com",
+		"https://github.com",
+	}
+
+
+	for _, web := range websiteList {
+		go getStatusCode(web)
+		wg.Add(1)
+	}
+
+	wg.Wait()
 }
 
-func greeter(s string) {
-	for i := 0; i < 6; i++ {
-		time.Sleep(3*time.Millisecond)
-		fmt.Println(s)
+// func greeter(s string) {
+// 	for i := 0; i < 6; i++ {
+// 		time.Sleep(3*time.Millisecond)
+// 		fmt.Println(s)
+// 	}
+// }
+
+func getStatusCode(endpoint string) {
+	defer wg.Done()
+	res, err := http.Get(endpoint)
+
+	if err != nil {
+		fmt.Println("oops in endpoint")
 	}
+	fmt.Printf("%d status code for website %s\n", res.StatusCode, endpoint)
 }
